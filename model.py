269198d0240ec72fd9a1a52992ad8820dd9220ef
Taha Abdullah <106115454+tmabdull@users.noreply.github.com>
2023-11-04 18:29:42 -0700

    modified gpt prompt to get 21 recipes

--- model.py
+++ model.py
@@ -6,7 +6,7 @@ import openai
 load_dotenv()
 openai.api_key = os.getenv('API_KEY')
 
-def generate_recipe(location):
+def generate_recipe(location, cuisine):
     """
     This function takes a list of ingredients and generates a recipe using OpenAI's GPT-4 API.
     
@@ -14,14 +14,14 @@ def generate_recipe(location):
     :return: A string containing the generated recipe.
     """
     
-    prompt = f"I live in {location}. Can you create a recipe for me?"
+    prompt = f"I am a college student in {location} who prefers {cuisine} cuisine. Generate a list of 21 recipes that I can follow, keeping the location in mind."
 
     try:
         # Call the OpenAI API
         response = openai.Completion.create(
             model="text-davinci-003",  # Use an appropriate model; "text-davinci-003" is an example
             prompt=prompt,
-            max_tokens=150  # You can adjust the number of max_tokens as needed
+            max_tokens=300  # You can adjust the number of max_tokens as needed
         )
         # Extract the recipe from the response
         recipe = response.choices[0].text.strip()
@@ -35,6 +35,6 @@ def generate_recipe(location):
 
 # Example usage:
 location = "Davis, CA"
-
-recipe = generate_recipe("Davis, CA")
+cuisine = "Meditterranean"
+recipe = generate_recipe(location, cuisine)
 print(recipe)
